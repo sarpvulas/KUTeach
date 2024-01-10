27948947cb5f6141f70c25c5479d520057e82cf1
Sarp Vulaş <sarpvulas@gmail.com>
2024-01-10 16:49:21 +0300

    Initial Commit

---
+++ KUTeach/KUTeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUTeachApp.swift
+//  KUTeach
+//
+//  Created by Sarp Vulaş on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KUTeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
